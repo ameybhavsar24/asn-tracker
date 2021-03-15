4b78e2c2196958ad2e47b020cd277d848c4d4025
Amey Bhavsar <iamamey24@gmail.com>
2021-03-15 17:37:05 +0530

    added login and dashboard

--- public/nav.php
+++ public/nav.php
@@ -1,3 +1,11 @@
+<?php
+  session_start();
+  $name = "";
+  if ($_SESSION["loggedin"]) {
+    $name = $_SESSION["name"];
+  }
+?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand mb-0 h1" href="/">Assignment Tracker</a>
 
@@ -15,18 +23,36 @@
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
     <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="login.php">Login<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="register.php">Register<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="about.php">About<span class="sr-only">(current)</span></a>
-      </li>
+      <?php
+      if ($_SESSION["loggedin"]) {
+      ?>
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="profile.php">Hello, <?= $name ?><span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="logout.php">Logout<span class="sr-only">(current)</span></a>
+        </li>
+      <?php
+      } else {
+      ?>
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="login.php">Login<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="register.php">Register<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="about.php">About<span class="sr-only">(current)</span></a>
+        </li>
+      <?php
+      }
+      ?>
     </ul>
   </div>
 </nav>
